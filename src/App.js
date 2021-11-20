2dd7373d9aa75f964283cf24a1a0327808c88861
Saimur Rahman <saimur715@gmail.com>
2021-11-20 14:54:19 +0600

    project finished

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { Typography, AppBar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import VideoPlayer from './components/VideoPlayer';
+// import Sidebar from './components/Sidebar';
+import Notifications from './components/Notifications';
+import Options from './components/Options';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography variant='h2' align='center'>
+          Chat application
+        </Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+      {/* <Sidebar>
+        <Notifications />
+      </Sidebar> */}
+    </div>
+  );
+}
+
+export default App;
